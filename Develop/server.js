1bfe1eb74d9bdda4d059d6e7059cc8018f4e1801
SafteyDav3 <talktomedw@gmail.com>
2022-06-24 13:05:21 -0500

    fist commit

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const routes = require('./routes');
+const express = require("express");
+const routes = require("./routes");
 // import sequelize connection
+const sequelize = require("sequelize");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,6 +12,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+sequelize.sync({ force: true }).then(function () {
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+  });
 });
